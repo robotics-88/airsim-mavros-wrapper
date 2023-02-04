faa39f9153e3692a980758c2a2497bbf69201b39
ErinLine <erinline@gmail.com>
2023-02-04 14:23:05 -0500

    Fixed another quaternion

--- src/airsim_ros_wrapper.cpp
+++ src/airsim_ros_wrapper.cpp
@@ -670,10 +670,13 @@ void AirsimRosWrapper::publishOdomTf(const nav_msgs::Odometry& odom_msg)
     odom_tf.transform.translation.x = odom_msg.pose.pose.position.x;
     odom_tf.transform.translation.y = odom_msg.pose.pose.position.y;
     odom_tf.transform.translation.z = odom_msg.pose.pose.position.z;
-    odom_tf.transform.rotation.x = odom_msg.pose.pose.orientation.x;
-    odom_tf.transform.rotation.y = odom_msg.pose.pose.orientation.y;
-    odom_tf.transform.rotation.z = odom_msg.pose.pose.orientation.z;
-    odom_tf.transform.rotation.w = odom_msg.pose.pose.orientation.w;
+    tf2::Quaternion quat;
+    quat.setRPY(odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y, odom_msg.pose.pose.orientation.z);
+    quat.normalize();
+    odom_tf.transform.rotation.x = quat.x();
+    odom_tf.transform.rotation.y = quat.y();
+    odom_tf.transform.rotation.z = quat.z();
+    odom_tf.transform.rotation.w = quat.w();
     tf_broadcaster_.sendTransform(odom_tf);
 }
 
